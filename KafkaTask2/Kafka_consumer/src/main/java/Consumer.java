6be488d8f711996f13e374d8186d8f0213a8a59f
LabaLabaDabDab <s.khomchenko@g.nsu.ru>
2024-07-21 18:56:50 +0700

    add lab2

---
+++ KafkaTask2/Kafka_consumer/src/main/java/Consumer.java
@@ -0,0 +1,54 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+    private static final Logger logger = LogManager.getLogger(Consumer.class);
+    private static int currentSum = 0;
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            logger.error("Please provide the consumer instance number and partition as arguments.");
+            System.exit(1);
+        }
+
+        String consumerInstance = args[0];
+        int partitionId = Integer.parseInt(args[1]);
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "shop-events-group");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+
+        try (consumer) {
+            TopicPartition partition = new TopicPartition("shop-events", partitionId);
+            consumer.assign(Collections.singletonList(partition));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records) {
+                    String[] values = record.value().split(",");
+                    int price = Integer.parseInt(values[1]);
+                    currentSum += price;
+                    String updatedMessage = values[0] + "," + currentSum + "," + values[2];
+                    System.out.println(consumerInstance + ": " + updatedMessage);
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Unexpected error", e);
+        }
+    }
+}
---
+++ KafkaTask2/Kafka_producer/src/main/java/Producer.java
@@ -0,0 +1,72 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Producer {
+    private static final Logger logger = LogManager.getLogger(String.valueOf(Producer.class));
+
+    public static void main(String[] args) throws IOException {
+        String topicName = "shop-events";
+        String csvFile = "Kafka_producer\\sales1.txt";
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        Set<String> uniqueKeys = new HashSet<>();
+        collectUniqueKeys(csvFile, uniqueKeys);
+
+        Map<String, Integer> partitionMap = assignPartitions(uniqueKeys);
+
+        publishFileToTopic(producer, topicName, csvFile, partitionMap);
+
+        logger.info("Producer application finished.");
+
+        producer.flush();
+        producer.close();
+    }
+
+    private static void collectUniqueKeys(String csvFile, Set<String> uniqueKeys) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String key = line.split(",")[0];
+                uniqueKeys.add(key);
+            }
+        }
+    }
+
+    private static Map<String, Integer> assignPartitions(Set<String> uniqueKeys) {
+        Map<String, Integer> partitionMap = new HashMap<>();
+        int partition = 0;
+        for (String key : uniqueKeys) {
+            partitionMap.put(key, partition);
+            partition = (partition + 1) % 4;
+        }
+        return partitionMap;
+    }
+
+    private static void publishFileToTopic(KafkaProducer<String, String> producer, String topicName, String csvFile, Map<String, Integer> partitionMap) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String key = line.split(",")[0];
+                int partition = partitionMap.get(key);
+                producer.send(new ProducerRecord<>(topicName, partition, key, line));
+                logger.info("Sent record to topic {} with key {}: {}", topicName, key, line);
+            }
+        }
+    }
+}
