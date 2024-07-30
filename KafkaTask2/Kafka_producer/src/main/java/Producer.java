c95feac0dfc697065568228ca6076665390fee76
LabaLabaDabDab <s.khomchenko@g.nsu.ru>
2024-07-30 12:19:06 +0700

    Update Producer.java
    delete trash

--- KafkaTask2/Kafka_producer/src/main/java/Producer.java
+++ KafkaTask2/Kafka_producer/src/main/java/Producer.java
@@ -13,7 +13,7 @@ import java.util.Properties;
 public class Producer {
     private static final Logger logger = LogManager.getLogger(String.valueOf(Producer.class));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String topicName = "shop-events";
         String csvFile = "Kafka_producer\\sales1.txt";
 
@@ -42,36 +42,4 @@ public class Producer {
         producer.flush();
         producer.close();
     }
-
-/*    private static void collectUniqueKeys(String csvFile, Set<String> uniqueKeys) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String key = line.split(",")[0];
-                uniqueKeys.add(key);
-            }
-        }
-    }
-
-    private static Map<String, Integer> assignPartitions(Set<String> uniqueKeys) {
-        Map<String, Integer> partitionMap = new HashMap<>();
-        int partition = 0;
-        for (String key : uniqueKeys) {
-            partitionMap.put(key, partition);
-            partition = (partition + 1) % 4;
-        }
-        return partitionMap;
-    }
-
-    private static void publishFileToTopic(KafkaProducer<String, String> producer, String topicName, String csvFile, Map<String, Integer> partitionMap) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String key = line.split(",")[0];
-                int partition = partitionMap.get(key);
-                producer.send(new ProducerRecord<>(topicName, partition, key, line));
-                logger.info("Sent record to topic {} with key {}: {}", topicName, key, line);
-            }
-        }
-    }*/
 }
